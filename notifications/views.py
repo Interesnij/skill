674649385773fce0fa3daa04d774baffb451be74
Serg <ochkarik1983@mail.ru@>
2020-04-09 13:21:44 +0300

    N

--- notifications/views.py
+++ notifications/views.py
@@ -2,5 +2,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 
 
-class ItemNotificationListView(LoginRequiredMixin, TEmplateView):
+class ItemNotificationListView(LoginRequiredMixin, TemplateView):
     template_name = 'user_notify_list.html'
