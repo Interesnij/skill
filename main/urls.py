ab632706bab5c857b5ebc9602614d74af5775747
Serg <ochkarik1983@mail.ru@>
2020-04-09 22:20:02 +0300

    h

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from main.views import MainPageView
 
 
 urlpatterns = [
-	url(r'^$', MainPageView.as_view(), name="coming"),
+	url(r'^$', MainPageView.as_view(), name="main"),
+	url(r'test^$', TestPageView.as_view(), name="test"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.views.generic.base import TemplateView
 
 class MainPageView(TemplateView):
 	template_name='main/mainpage.html'
+
+class TestPageView(TemplateView):
+	template_name='main/test.html'
