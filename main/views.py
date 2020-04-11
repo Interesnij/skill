92fa983df100e18478da7a08eeabcda3d8189be4
Serg <ochkarik1983@mail.ru@>
2020-04-11 13:21:42 +0300

    H

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 
 
 class MainPageView(TemplateView):
-	template_name='main/mainpage.html'
+	template_name='main/test.html'
 
 class TestPageView(TemplateView):
 	template_name='main/test.html'
