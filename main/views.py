719aea2459955511c6822e6bddd9884445935469
Serg <ochkarik1983@mail.ru@>
2020-04-13 15:38:24 +0300

    H

--- main/views.py
+++ main/views.py
@@ -5,5 +5,16 @@ class MainPageView(TemplateView):
 	template_name = None
 
 	def get(self,request,*args,**kwargs):
-		self.template_name = request.user.get_template_main(folder="main/", template="page.html", request=request)
+		import re
+
+		if request.user.is_authenticated and not request.user.is_deleted:
+			self.template_name = "main/page.html"
+		elif request.user.is_authenticated and request.user.is_deleted:
+			self.template_name = "generic/user_deleted.html"
+		elif request.user.is_anonymous:
+			self.template_name = "main/page.html"
+
+		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+        if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+            self.template_name = "mob_" + self.template_name
 		return super(MainPageView,self).get(request,*args,**kwargs)
