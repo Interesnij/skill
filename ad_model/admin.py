d4b60b4fd9f13c853e6e059f1f3feb55cde9186c
Serg <ochkarik1983@mail.ru@>
2020-04-13 10:41:30 +0300

    H

--- ad_model/admin.py
+++ ad_model/admin.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
-from model.auto import Ad, Auto
-from model.spez_moto import SpecMoto
-from model.zap_auto import ZapAuto
-from model.realty import Realty
-from model.service import Service
-from model.job import Job
-from model.women import Women
-from model.man import Man
-from model.child import Child
-from model.baby import Baby
-from model.phone import PhoneAd
-from model.foto_video import FotoVideo
-from model.home_tech import HomeTech
-from model.tv import TV
-from model.comp import Comp
-from model.home_garden import HomeGarden
-from model.repair import Repair
-from model.beauty import Beauty
-from model.sport import Sport
-from model.hobbies import Hobbies
-from model.pets import Pets
-from model.biznes import Biznes
+from ad_model.auto import Ad, Auto
+from ad_model.spez_moto import SpecMoto
+from ad_model.zap_auto import ZapAuto
+from ad_model.realty import Realty
+from ad_model.service import Service
+from ad_model.job import Job
+from ad_model.women import Women
+from ad_model.man import Man
+from ad_model.child import Child
+from ad_model.baby import Baby
+from ad_model.phone import PhoneAd
+from ad_model.foto_video import FotoVideo
+from ad_model.home_tech import HomeTech
+from ad_model.tv import TV
+from ad_model.comp import Comp
+from ad_model.home_garden import HomeGarden
+from ad_model.repair import Repair
+from ad_model.beauty import Beauty
+from ad_model.sport import Sport
+from ad_model.hobbies import Hobbies
+from ad_model.pets import Pets
+from ad_model.biznes import Biznes
 
 
 admin.site.register(Auto)
