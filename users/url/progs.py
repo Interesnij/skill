254ee8e9af9d7e4bd021a541aac6713a8c1cc9ec
Serg <ochkarik1983@mail.ru@>
2020-04-13 19:21:28 +0300

    H

--- users/url/progs.py
+++ users/url/progs.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
-from users.view.progs import UserBanCreate, UserUnbanCreate, UserItemView, PhoneSend, PhoneVerify
+from users.view.progs import UserBanCreate, UserUnbanCreate, UserAdView, PhoneSend, PhoneVerify
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     url(r'^block/(?P<pk>\d+)/$', login_required(UserBanCreate.as_view())),
     url(r'^unblock/(?P<pk>\d+)/$', login_required(UserUnbanCreate.as_view())),
-    url(r'^item_view/(?P<pk>\d+)/$', login_required(UserItemView.as_view())),
+    url(r'^item_view/(?P<pk>\d+)/$', login_required(UserAdView.as_view())),
     url(r'^phone_send/(?P<phone>\d+)/$', login_required(PhoneSend.as_view())),
     url(r'^phone_verify/(?P<phone>\d+)/(?P<code>\d+)/$', login_required(PhoneVerify.as_view())),
 ]
