bbd606ebdcc01403e74ef2369a320ef491d00479
Serg <ochkarik1983@mail.ru@>
2020-04-13 12:03:06 +0300

    H

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from users.model.profile import UserProfile, IPUser, OneUserLocation, TwoUserLocation, ThreeUserLocation
 from users.model.list import Favourites, ChatAd, Subscribe, Guest
-from users.model import User
+from django.conf import settings
 
-admin.site.register(User)
+
+admin.site.register(settings.AUTH_USER_MODEL)
 
 admin.site.register(UserProfile)
 admin.site.register(IPUser)
