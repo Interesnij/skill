cc17134028e927cb5bffce6ab436ecc3762e0a80
Serg <ochkarik1983@mail.ru@>
2020-04-13 10:36:14 +0300

    H

--- regions/models.py
+++ regions/models.py
@@ -4,10 +4,10 @@ from countries.models import Country
 
 class Region(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE, verbose_name="Страна")
-	name_ru = models.CharField(max_length=100, verbose_name="Русское название")
-	name_en = models.CharField(max_length=100, verbose_name="Латинское название")
-	order = models.PositiveSmallIntegerField(default=0, verbose_name="Порядковый номер")
-	image = models.ImageField(blank=True, verbose_name="Изображение", upload_to="regions")
+    name_ru = models.CharField(max_length=100, verbose_name="Русское название")
+    name_en = models.CharField(max_length=100, verbose_name="Латинское название")
+    order = models.PositiveSmallIntegerField(default=0, verbose_name="Порядковый номер")
+    image = models.ImageField(blank=True, verbose_name="Изображение", upload_to="regions")
 
 	def __str__(self):
 		return self.name_ru
