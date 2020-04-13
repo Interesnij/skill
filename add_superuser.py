a05571b522695ba9cbfa14f23c029617d530977a
Serg <ochkarik1983@mail.ru@>
2020-04-13 11:11:02 +0300

    H

---
+++ add_superuser.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from locale import *
+import sys,os
+
+project_dir = '../skill/skill/'
+
+sys.path.append(project_dir)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import django
+django.setup()
+
+from django.conf import settings
+from users.models import User
+
+
+User.objects.create(id=1,
+                    username="Serg",
+                    phone="79042373637",
+                    password="ulihos46",
+                    email="ochkarik1983@mail.ru",
+                    is_superuser=True)
