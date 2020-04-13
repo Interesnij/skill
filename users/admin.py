d34264da525e199255f963086ea7ea6c8f6e060d
Serg <ochkarik1983@mail.ru@>
2020-04-13 12:01:24 +0300

    H

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from users.model.profile import UserProfile, IPUser, OneUserLocation, TwoUserLocation, ThreeUserLocation
+from users.model.list import Favourites, ChatAd, Subscribe, Guest
+from users.model import User
+
+admin.site.register(User)
+
+admin.site.register(UserProfile)
+admin.site.register(IPUser)
+admin.site.register(OneUserLocation)
+admin.site.register(TwoUserLocation)
+admin.site.register(ThreeUserLocation)
+
+admin.site.register(Favourites)
+admin.site.register(ChatAd)
+admin.site.register(Subscribe)
+admin.site.register(Guest)
--- users/model/profile.py
+++ users/model/profile.py
@@ -17,7 +17,7 @@ class UserProfile(models.Model):
     facebook_url = models.URLField(blank=True, verbose_name="Ссылка на facebook")
     instagram_url = models.URLField(blank=True, verbose_name="Ссылка на instagram")
     twitter_url = models.URLField(blank=True, verbose_name="Ссылка на twitter")
-    avatar = ProcessedImageField(format='JPEG', options={'quality': 80}, upload_to=upload_to_user_directory, processors=[ResizeToFit(width=500, upscale=500)]) 
+    avatar = ProcessedImageField(format='JPEG', options={'quality': 80}, upload_to=upload_to_user_directory, processors=[ResizeToFit(width=500, upscale=500)])
 
 
     def __str__(self):
@@ -35,3 +35,84 @@ class UserProfile(models.Model):
         verbose_name = 'Профиль пользователя'
         verbose_name_plural = 'Профили пользователей'
         index_together = [('id', 'user'),]
+
+
+class OneUserLocation(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="user_location", verbose_name="Пользователь", on_delete=models.CASCADE)
+    city_ru = models.CharField(max_length=100, blank=True, verbose_name="Город по-русски")
+    city_en = models.CharField(max_length=100, blank=True, verbose_name="Город по-английски")
+    city_lat = models.FloatField(blank=True, null=True, verbose_name="Ширина города")
+    city_lon = models.FloatField(blank=True, null=True, verbose_name="Долгота города")
+    region_ru = models.CharField(max_length=100, blank=True, verbose_name="Регион по-русски")
+    region_en = models.CharField(max_length=100, blank=True, verbose_name="Регион по-английски")
+    country_ru = models.CharField(max_length=100, blank=True, verbose_name="Страна по-русски")
+    country_en = models.CharField(max_length=100, blank=True, verbose_name="Страна по-английски")
+    phone = models.CharField(max_length=5, blank=True, verbose_name="Начало номера")
+
+    class Meta:
+        verbose_name="Местоположение 1"
+        verbose_name_plural="Местоположения 1"
+        index_together = [('id', 'user'),]
+
+    def __str__(self):
+        return '{}, {}, {}'.format(self.country_ru, self.region_ru, self.city_ru)
+        
+    def get_sity(self):
+        return self.city_ru
+
+
+class TwoUserLocation(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="user_location_2", verbose_name="Пользователь", on_delete=models.CASCADE)
+    city_ru = models.CharField(max_length=100, blank=True, verbose_name="Город по-русски")
+    city_en = models.CharField(max_length=100, blank=True, verbose_name="Город по-английски")
+    city_lat = models.FloatField(blank=True, null=True, verbose_name="Ширина города")
+    city_lon = models.FloatField(blank=True, null=True, verbose_name="Долгота города")
+    region_ru = models.CharField(max_length=100, blank=True, verbose_name="Регион по-русски")
+    region_en = models.CharField(max_length=100, blank=True, verbose_name="Регион по-английски")
+    country_ru = models.CharField(max_length=100, blank=True, verbose_name="Страна по-русски")
+    country_en = models.CharField(max_length=100, blank=True, verbose_name="Страна по-английски")
+    phone = models.CharField(max_length=5, blank=True, verbose_name="Начало номера")
+
+    class Meta:
+        verbose_name="Местоположение 2"
+        verbose_name_plural="Местоположения 2"
+        index_together = [('id', 'user'),]
+
+    def __str__(self):
+        return '{}, {}, {}'.format(self.country_ru, self.region_ru, self.city_ru)
+
+
+class ThreeUserLocation(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="user_location_3", verbose_name="Пользователь", on_delete=models.CASCADE)
+    city_ru = models.CharField(max_length=100, blank=True, verbose_name="Город по-русски")
+    city_en = models.CharField(max_length=100, blank=True, verbose_name="Город по-английски")
+    city_lat = models.FloatField(blank=True, null=True, verbose_name="Ширина города")
+    city_lon = models.FloatField(blank=True, null=True, verbose_name="Долгота города")
+    region_ru = models.CharField(max_length=100, blank=True, verbose_name="Регион по-русски")
+    region_en = models.CharField(max_length=100, blank=True, verbose_name="Регион по-английски")
+    country_ru = models.CharField(max_length=100, blank=True, verbose_name="Страна по-русски")
+    country_en = models.CharField(max_length=100, blank=True, verbose_name="Страна по-английски")
+    phone = models.CharField(max_length=5, blank=True, verbose_name="Начало номера")
+
+    class Meta:
+        verbose_name="Местоположение 3"
+        verbose_name_plural="Местоположения 3"
+        index_together = [('id', 'user'),]
+
+    def __str__(self):
+        return '{}, {}, {}'.format(self.country_ru, self.region_ru, self.city_ru)
+
+
+class IPUser(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="user_ip", verbose_name="Пользователь", on_delete=models.CASCADE)
+    ip_1 = models.GenericIPAddressField(protocol='both', null=True, blank=True, verbose_name="ip 1")
+    ip_2 = models.GenericIPAddressField(protocol='both', null=True, blank=True, verbose_name="ip 2")
+    ip_3 = models.GenericIPAddressField(protocol='both', null=True, blank=True, verbose_name="ip 3")
+
+    class Meta:
+        verbose_name="ip пользователя"
+        verbose_name_plural="ip пользователей"
+        index_together = [('id', 'user'),]
+
+    def __str__(self):
+        return '{} - {}, {}, {}'.format(self.user.get_full_name(), self.ip_1, self.ip_2, self.ip_3)
