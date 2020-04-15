b96dae9c13d2210a03ee12569a279e74f06c6686
Serg <ochkarik1983@mail.ru@>
2020-04-15 20:01:28 +0300

    H

--- ad_categories/urls.py
+++ ad_categories/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^region_(?P<region_name>[\w\-]+)/(?P<cat_name>[\w\-]+)/$', AdRegionCategoryView.as_view(), name='ad_category_region'),
     url(r'^sity_(?P<city_name>[\w\-]+)/(?P<cat_name>[\w\-]+)/$', AdCityCategoryView.as_view(), name='ad_category_city'),
 
-    url(r'^sub_(?P<subcat_name>[\w\-]+)/$', AdSubCategoryView.as_view(), name='ad_subcategory'),
+    url(r'^(?P<subcat_name>[\w\-]+)/$', AdSubCategoryView.as_view(), name='ad_subcategory'),
     url(r'^reg_(?P<region_name>[\w\-]+)/name_(?P<subcat_name>[\w\-]+)/$', AdRegionSubCategoryView.as_view(), name='ad_subcategory_region'),
     url(r'^(?P<city_name>[\w\-]+)/sub_(?P<subcat_name>[\w\-]+)/$', AdCitySubCategoryView.as_view(), name='ad_subcategory_city'),
 ]
