47598d1d8904df5aed6e02385b37cc1c0988cb46
Serg <ochkarik1983@mail.ru@>
2020-04-15 17:57:23 +0300

    H

--- ad_categories/urls.py
+++ ad_categories/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     url(r'^$', AdCategoriesView.as_view(), name='ad_categories'),
 
     url(r'^(?P<cat_name>[\w\-]+)/$', AdCategoryView.as_view(), name='ad_category'),
-    url(r'^(?P<region_name>[\w\-]+)/(?P<cat_name>[\w\-]+)/$', AdRegionCategoryView.as_view(), name='ad_category_region'),
-    url(r'^(?P<city_name>[\w\-]+)/(?P<cat_name>[\w\-]+)/$', AdCityCategoryView.as_view(), name='ad_category_city'),
+    url(r'^region_(?P<region_name>[\w\-]+)/(?P<cat_name>[\w\-]+)/$', AdRegionCategoryView.as_view(), name='ad_category_region'),
+    url(r'^sity_(?P<city_name>[\w\-]+)/(?P<cat_name>[\w\-]+)/$', AdCityCategoryView.as_view(), name='ad_category_city'),
 
-    url(r'^(?P<subcat_name>[\w\-]+)/$', AdSubCategoryView.as_view(), name='ad_subcategory'),
-    url(r'^(?P<region_name>[\w\-]+)/(?P<subcat_name>[\w\-]+)/$', AdRegionSubCategoryView.as_view(), name='ad_subcategory_region'),
-    url(r'^(?P<city_name>[\w\-]+)/(?P<subcat_name>[\w\-]+)/$', AdCitySubCategoryView.as_view(), name='ad_subcategory_city'),
+    url(r'^sub_(?P<subcat_name>[\w\-]+)/$', AdSubCategoryView.as_view(), name='ad_subcategory'),
+    url(r'^reg_(?P<region_name>[\w\-]+)/name_(?P<subcat_name>[\w\-]+)/$', AdRegionSubCategoryView.as_view(), name='ad_subcategory_region'),
+    url(r'^sity_(?P<city_name>[\w\-]+)/sub_(?P<subcat_name>[\w\-]+)/$', AdCitySubCategoryView.as_view(), name='ad_subcategory_city'),
 ]
