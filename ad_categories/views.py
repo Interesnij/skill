5ce4f59675885773eaa3fd840dccb6f042a3750e
Serg <ochkarik1983@mail.ru@>
2020-04-15 10:07:56 +0300

    H

--- ad_categories/views.py
+++ ad_categories/views.py
@@ -13,16 +13,16 @@ class AdCategoryView(TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.cat=AdCategory.objects.get(name_en=self.kwargs["name_en"])
-		if request.user.is_authenticated and not request.user.is_deleted:
-			self.template_name = "ad_cat/page.html"
-		elif request.user.is_authenticated and request.user.is_deleted:
-			self.template_name = "generic/user_deleted.html"
-		elif request.user.is_anonymous:
-			self.template_name = "ad_cat/anon_page.html"
+        if request.user.is_authenticated and not request.user.is_deleted:
+            self.template_name = "ad_cat/page.html"
+        elif request.user.is_authenticated and request.user.is_deleted:
+            self.template_name = "generic/user_deleted.html"
+        elif request.user.is_anonymous:
+            self.template_name = "ad_cat/anon_page.html"
 
-		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
-		if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
-			self.template_name = "mob_" + self.template_name
+        MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+        if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+            self.template_name = "mob_" + self.template_name
         return super(AdCategoryView,self).get(request,*args,**kwargs)
 
     def get_context_data(self, **kwargs):
@@ -36,16 +36,16 @@ class AdSubCategoryView(TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.cat=AdSubCategory.objects.get(name_en=self.kwargs["name_en"])
-		if request.user.is_authenticated and not request.user.is_deleted:
-			self.template_name = "ad_subcat/page.html"
-		elif request.user.is_authenticated and request.user.is_deleted:
-			self.template_name = "generic/user_deleted.html"
-		elif request.user.is_anonymous:
-			self.template_name = "ad_subcat/anon_page.html"
+        if request.user.is_authenticated and not request.user.is_deleted:
+            self.template_name = "ad_subcat/page.html"
+        elif request.user.is_authenticated and request.user.is_deleted:
+            self.template_name = "generic/user_deleted.html"
+        elif request.user.is_anonymous:
+            self.template_name = "ad_subcat/anon_page.html"
 
-		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
-		if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
-			self.template_name = "mob_" + self.template_name
+        MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+        if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+            self.template_name = "mob_" + self.template_name
         return super(AdSubCategoryView,self).get(request,*args,**kwargs)
 
     def get_context_data(self, **kwargs):
