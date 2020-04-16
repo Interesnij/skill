2db32bf383d0f8d7114859d17bfddd28557103de
Serg <ochkarik1983@mail.ru@>
2020-04-16 10:54:36 +0300

    G

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from search.views import SearchView
+from search.views import SearchView, GetSubCat
 
 
 urlpatterns = [
     url(r'^$', SearchView.as_view(), name='search'),
+    url(r'get_subcat/(?P<pk>\d+)/$', GetSubCat.as_view()),
 ]
--- search/views.py
+++ search/views.py
@@ -1,5 +1,20 @@
 from django.views.generic.base import TemplateView
+from ad_categories.models import AdCategory, AdSubCategory
 
 
 class SearchView(TemplateView):
     template_name = "search.html"
+
+
+class GetSubCat(View):
+    template_name = "get_subcat.html"
+
+    def get(self,request,*args,**kwargs):
+        self.cat = AdCategory.objects.get(pk=self.kwargs["pk"])
+        self.subcats = AdSubCategory.objects.filter(category=self.cat)
+        return super(GetSubCat,self).get(request,*args,**kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(CityView, self).get_context_data(**kwargs)
+        context['subcats'] = self.subcats
+        return context
