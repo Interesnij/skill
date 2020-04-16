b39f8af9e6dbe3b881882d416c40d10bf0088529
Serg <ochkarik1983@mail.ru@>
2020-04-16 17:48:39 +0300

    G

--- users/models.py
+++ users/models.py
@@ -175,7 +175,7 @@ class User(AbstractUser):
     def get_subscribs(self):
         """Это те, кто на меня подписался"""
 
-        subscribs_query = Q(adding_user__adding_user__id=self.id, is_deleted=False)
+        subscribs_query = Q(added_user__added_user__id=self.id, is_deleted=False)
         subscribs_query.add(~Q(Q(blocked_by_users__blocker_id=self.id) | Q(user_blocks__blocked_user_id=self.id)), Q.AND)
         subscribs = User.objects.filter(subscribs_query)
         return subscribs
@@ -183,7 +183,7 @@ class User(AbstractUser):
     def get_my_subscribs(self):
         """Это те, на кого я подписался"""
 
-        subscribs_query = Q(added_user__added_user__id=self.id, is_deleted=False)
+        subscribs_query = Q(adding_user__adding_user__id=self.id, is_deleted=False)
         subscribs_query.add(~Q(Q(blocked_by_users__blocker_id=self.id) | Q(user_blocks__blocked_user_id=self.id)), Q.AND)
         subscribs = User.objects.filter(subscribs_query)
         return subscribs
