82713c2050db4962afc03d7e7f511b1366e27fdc
Serg <ochkarik1983@mail.ru@>
2020-04-16 13:44:02 +0300

    G

--- static/scripts/1_post.js
+++ static/scripts/1_post.js
@@ -1,5 +1,4 @@
- on('body', 'click', '.ad_sold', function(e) {
-   e.preventDefault;
+ on('body', 'click', '.ad_sold', function() {
    item = this.parentElement.parentElement.parentElement.parentElement;
    pk = item.getAttribute("data-pk");
    parent = this.parentElement;
@@ -9,7 +8,7 @@
    if ( link.readyState == 4 && link.status == 200 ) {
      new_span = document.createElement("span");
      new_span.classList.add("ad_unsold");
-     new_span.innerHTML = "Восстановить";
+     new_span.innerHTML = "Активировать";
      parent.innerHTML = "";
      parent.append(new_span);
    }};
@@ -26,6 +25,40 @@ on('body', 'click', '.ad_unsold', function() {
   if ( link.readyState == 4 && link.status == 200 ) {
     new_span = document.createElement("span");
     new_span.classList.add("ad_sold");
+    new_span.innerHTML = "Продано";
+    parent.innerHTML = "";
+    parent.append(new_span);
+  }};
+  link.send( null );
+})
+
+on('body', 'click', '.ad_remove', function() {
+  item = this.parentElement.parentElement.parentElement.parentElement;
+  pk = item.getAttribute("data-pk");
+  parent = this.parentElement;
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'GET', "/users/ad_progs/delete/" + pk + "/", true );
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    new_span = document.createElement("span");
+    new_span.classList.add("ad_unremove");
+    new_span.innerHTML = "Восстановить";
+    parent.innerHTML = "";
+    parent.append(new_span);
+  }};
+  link.send( null );
+})
+
+on('body', 'click', '.ad_unremove', function() {
+  item = this.parentElement.parentElement.parentElement.parentElement;
+  pk = item.getAttribute("data-pk");
+  parent = this.parentElement;
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'GET', "/users/ad_progs/undelete/" + pk + "/", true );
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    new_span = document.createElement("span");
+    new_span.classList.add("ad_remove");
     new_span.innerHTML = "Удалить";
     parent.innerHTML = "";
     parent.append(new_span);
