7be9723ba535890576898c1c49e8db184290a1c6
Serg <ochkarik1983@mail.ru@>
2020-04-17 10:06:17 +0300

    G

--- ad_posts/views.py
+++ ad_posts/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from ad_posts.models import Ad
+from common.utils import get_template_ad_detail
 
 
 class AdPostsView(TemplateView):
@@ -12,7 +13,7 @@ class AdDetailView(TemplateView):
     def get(self,request,*args,**kwargs):
         self.user = request.user
         self.ad = Ad.objects.get(pk=self.kwargs["pk"])
-        self.template_name = request.user.get_template_ad_detail(ad=self.ad, folder="detail/", template="_page.html", request=request)
+        self.template_name = get_template_ad_detail(ad=self.ad, folder="detail/", template="_page.html", request=request)
         return super(AdDetailView,self).get(request,*args,**kwargs)
 
     def get_context_data(self, **kwargs):
--- users/models.py
+++ users/models.py
@@ -71,41 +71,6 @@ class User(AbstractUser):
                 UserNumbers.objects.create(visitor=request.user.pk, target=self.pk, platform=0)
         return template_name
 
-    def get_template_ad_detail(ad, folder, template, request):
-        import re
-        from stst.models import AdNumbers
-
-        cat_pk = str(ad.category.category.pk)
-        template = cat_pk + template
-        if ad.creator.pk == request.user.pk:
-            if not request.user.is_phone_verified:
-                template_name = "main/phone_verification.html"
-            else:
-                template_name = folder + "my_" + template
-        elif request.user.pk != ad.creator.pk and request.user.is_authenticated:
-            if not request.user.is_phone_verified:
-                template_name = "main/phone_verification.html"
-            elif request.user.is_blocked_with_user_with_id(user_id=self.pk):
-                template_name = folder + "block_" + template
-            elif request.user.is_deleted:
-                template_name = "main/deleted_page.html"
-            elif request.user.is_blocked:
-                template_name = "main/blocked_page.html"
-            else:
-                template_name = folder + template
-        elif request.user.is_anonymous:
-            template_name = folder + "anon_" + template
-
-        MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
-        if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
-            template_name = "mob_" + template_name
-            if request.user.is_authenticated:
-                AdNumbers.objects.create(user=request.user.pk, ad=ad.pk, platform=1)
-        else:
-            if request.user.is_authenticated:
-                AdNumbers.objects.create(user=request.user.pk, ad=ad.pk, platform=0)
-        return AdNumbers
-
     def get_my_template(self, folder, template, request):
         import re
         if self.pk == request.user.pk:
