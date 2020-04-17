6dd5165f6c32f9118e093d3a0d79986fdb2d25e7
Serg <ochkarik1983@mail.ru@>
2020-04-17 10:02:23 +0300

    G

--- users/models.py
+++ users/models.py
@@ -71,18 +71,18 @@ class User(AbstractUser):
                 UserNumbers.objects.create(visitor=request.user.pk, target=self.pk, platform=0)
         return template_name
 
-    def get_template_ad_detail(self, ad, folder, template, request):
+    def get_template_ad_detail(ad, folder, template, request):
         import re
         from stst.models import AdNumbers
 
         cat_pk = str(ad.category.category.pk)
         template = cat_pk + template
-        if self.pk == request.user.pk:
+        if ad.creator.pk == request.user.pk:
             if not request.user.is_phone_verified:
                 template_name = "main/phone_verification.html"
             else:
                 template_name = folder + "my_" + template
-        elif request.user.pk != self.pk and request.user.is_authenticated:
+        elif request.user.pk != ad.creator.pk and request.user.is_authenticated:
             if not request.user.is_phone_verified:
                 template_name = "main/phone_verification.html"
             elif request.user.is_blocked_with_user_with_id(user_id=self.pk):
@@ -99,11 +99,11 @@ class User(AbstractUser):
         MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
         if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
             template_name = "mob_" + template_name
-            if self.is_authenticated:
-                AdNumbers.objects.create(user=1, ad=1, platform=1)
+            if request.user.is_authenticated:
+                AdNumbers.objects.create(user=request.user.pk, ad=ad.pk, platform=1)
         else:
-            if self.is_authenticated:
-                AdNumbers.objects.create(user=1, ad=1, platform=0)
+            if request.user.is_authenticated:
+                AdNumbers.objects.create(user=request.user.pk, ad=ad.pk, platform=0)
         return AdNumbers
 
     def get_my_template(self, folder, template, request):
