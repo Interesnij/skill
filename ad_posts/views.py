43870493a8e8f45ad2d6ad975a6fc3fcc7e84c1d
Serg <ochkarik1983@mail.ru@>
2020-04-18 18:39:13 +0300

    J

--- ad_posts/views.py
+++ ad_posts/views.py
@@ -40,9 +40,8 @@ class FormAdd(TemplateView):
 
     def get(self,request,*args,**kwargs):
         self.subcat = AdSubCategory.objects.get(pk=self.kwargs["pk"])
-        form_import = "Form_" + str(self.subcat.category.order)
 
-        from ad_posts.forms import form_import as Form
+        from ad_posts.forms import "Form_" + str(self.subcat.category.order) as Form
 
         self.template_name = "forms/" + str(self.subcat.order) + ".html"
         self.form = Form(initial={"creator":request.user})
