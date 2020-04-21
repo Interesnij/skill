83f891adcc5ce1d19397d68b0ab454d9003f2909
Serg <ochkarik1983@mail.ru@>
2020-04-21 11:53:50 +0300

    J

--- users/templatetags/ad_count.py
+++ users/templatetags/ad_count.py
@@ -3,21 +3,21 @@ from django.template import Library
 register = Library()
 
 @register.filter
-def cat_region_count(cat, region):
-    count = cat.get_ads_in_region_count(region)
+def cat_region_count(cat, region_id):
+    count = cat.get_ads_in_region_count(region_id)
     return count
 
 @register.filter
-def cat_city_count(cat, city):
-    count = cat.get_ads_in_city_count(city)
+def cat_city_count(cat, city_id):
+    count = cat.get_ads_in_city_count(city_id)
     return count
 
 @register.filter
-def subcat_region_count(subcat, region):
-    count = subcat.get_ads_in_region_count(region)
+def subcat_region_count(subcat, region_id):
+    count = subcat.get_ads_in_region_count(region_id)
     return count
 
 @register.filter
-def subcat_city_count(subcat, city):
-    count = subcat.get_ads_in_city_count(city)
+def subcat_city_count(subcat, city_id):
+    count = subcat.get_ads_in_city_count(city_id)
     return count
