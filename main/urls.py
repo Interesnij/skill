d8195a8de5a6103f636b916fe15f2932212ce070
Serg <ochkarik1983@mail.ru@>
2020-04-23 18:43:54 +0300

    H

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
-from main.views import MainPageView, MainPhoneSend
+from main.views import MainPageView, AdsPageView, CoursesPageView, MainPhoneSend
 
 
 urlpatterns = [
 	url(r'^$', MainPageView.as_view(), name="main"),
+	url(r'ads/$', AdsPageView.as_view(), name="ads_main"),
+	url(r'courses/$', CoursesPageView.as_view(), name="courses_main"),
+
 	url(r'^phone_send/$', MainPhoneSend.as_view(), name="phone_sendd"),
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.views.generic.base import TemplateView
 from ad_categories.models import AdCategory
 
 
+
 class MainPageView(TemplateView):
 	template_name = None
 
@@ -9,11 +10,11 @@ class MainPageView(TemplateView):
 		import re
 
 		if request.user.is_authenticated and not request.user.is_deleted:
-			self.template_name = "main/page.html"
+			self.template_name = "main/main.html"
 		elif request.user.is_authenticated and request.user.is_deleted:
 			self.template_name = "generic/user_deleted.html"
 		elif request.user.is_anonymous:
-			self.template_name = "main/anon_page.html"
+			self.template_name = "main/anon_main.html"
 
 		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
 		if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
@@ -26,6 +27,56 @@ class MainPageView(TemplateView):
 		context = super(MainPageView, self).get_context_data(**kwargs)
 		context['ad_categories'] = AdCategory.objects.only("pk")
 		context['last_ads'] = last_ads_for_russia()
+		return context
+
+
+class AdsPageView(TemplateView):
+	template_name = None
+
+	def get(self,request,*args,**kwargs):
+		import re
+
+		if request.user.is_authenticated and not request.user.is_deleted:
+			self.template_name = "main/ads.html"
+		elif request.user.is_authenticated and request.user.is_deleted:
+			self.template_name = "generic/user_deleted.html"
+		elif request.user.is_anonymous:
+			self.template_name = "main/anon_ads.html"
+
+		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+		if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+			self.template_name = "mob_" + self.template_name
+		return super(AdsPageView,self).get(request,*args,**kwargs)
+
+	def get_context_data(self, **kwargs):
+		from common.utils import last_ads_for_russia
+
+		context = super(AdsPageView, self).get_context_data(**kwargs)
+		context['ad_categories'] = AdCategory.objects.only("pk")
+		context['last_ads'] = last_ads_for_russia()
+		return context
+
+
+class CoursesPageView(TemplateView):
+	template_name = None
+
+	def get(self,request,*args,**kwargs):
+		import re
+
+		if request.user.is_authenticated and not request.user.is_deleted:
+			self.template_name = "main/courses.html"
+		elif request.user.is_authenticated and request.user.is_deleted:
+			self.template_name = "generic/user_deleted.html"
+		elif request.user.is_anonymous:
+			self.template_name = "main/anon_courses.html"
+
+		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+		if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+			self.template_name = "mob_" + self.template_name
+		return super(CoursesPageView,self).get(request,*args,**kwargs)
+
+	def get_context_data(self, **kwargs):
+		context = super(CoursesPageView, self).get_context_data(**kwargs)
 		return context
 
 
