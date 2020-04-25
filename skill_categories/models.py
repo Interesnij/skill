e16242708a4301f16b5207921aa3c5845783dbd7
Serg <ochkarik1983@mail.ru@>
2020-04-25 20:28:55 +0300

    H

--- skill_categories/models.py
+++ skill_categories/models.py
@@ -30,27 +30,27 @@ class SkillCategory(models.Model):
 
 	def get_ads_in_region(self, region_id):
 		from skill_posts.models import Course
-	    ads_query = Q(category__category__id=self.id, city__region__id=region_id, creator__is_blocked=False, is_deleted=False, is_active=True)
-	    ads = Course.objects.filter(ads_query)
-	    return ads
+		ads_query = Q(category__category__id=self.id, city__region__id=region_id, creator__is_blocked=False, is_deleted=False, is_active=True)
+		ads = Course.objects.filter(ads_query)
+		return ads
 
 	def get_ads_in_region_count(self, region_id):
 		from skill_posts.models import Course
-	    ads_query = Q(category__category__id=self.id, city__region__id=region_id, creator__is_blocked=False, is_deleted=False, is_active=True)
-	    ads = Course.objects.filter(ads_query).values('id')
-	    return ads.count()
+		ads_query = Q(category__category__id=self.id, city__region__id=region_id, creator__is_blocked=False, is_deleted=False, is_active=True)
+		ads = Course.objects.filter(ads_query).values('id')
+		return ads.count()
 
 	def get_ads_in_city(self, city_id):
 		from skill_posts.models import Course
-	    ads_query = Q(category__category__id=self.id, city__id=city_id, creator__is_blocked=False, is_deleted=False, is_active=True)
-	    ads = Course.objects.filter(ads_query)
-	    return ads
+		ads_query = Q(category__category__id=self.id, city__id=city_id, creator__is_blocked=False, is_deleted=False, is_active=True)
+		ads = Course.objects.filter(ads_query)
+		return ads
 
 	def get_ads_in_city_count(self, city_id):
 		from skill_posts.models import Course
-	    ads_query = Q(category__category__id=self.id, city__id=city_id, creator__is_blocked=False, is_deleted=False, is_active=True)
-	    ads = Course.objects.filter(ads_query).values('id')
-	    return ads.count()
+		ads_query = Q(category__category__id=self.id, city__id=city_id, creator__is_blocked=False, is_deleted=False, is_active=True)
+		ads = Course.objects.filter(ads_query).values('id')
+		return ads.count()
 
 
 class SkillSubCategory(models.Model):
