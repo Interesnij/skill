4cb381f172c6b63ed5e4c05dbbf9975cb62efa3b
Serg <ochkarik1983@mail.ru@>
2020-04-26 13:59:02 +0300

    H

--- regions/urls.py
+++ regions/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
-from regions.views import RegionView, RegionCategoryView, RegionSubCategoryView
+from regions.views import RegionView
 
 
 urlpatterns = [
     url(r'^(?P<region_name>[\w\-]+)/$', RegionView.as_view(), name='region'),
-    url(r'^cat/(?P<region_name>[\w\-]+)/(?P<cat_name>[\w\-]+)/$', RegionCategoryView.as_view(), name='category_region'),
-    url(r'^subcat/(?P<region_name>[\w\-]+)/name_(?P<subcat_name>[\w\-]+)/$', RegionSubCategoryView.as_view(), name='subcategory_region'),
 ]
