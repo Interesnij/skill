60c3cc40df42e9dda73083768e4664a9424823b8
Serg <ochkarik1983@mail.ru@>
2020-04-27 08:16:50 +0300

    F

--- cities/models.py
+++ cities/models.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 class City(models.Model):
     region = models.ForeignKey(Region, on_delete=models.CASCADE, verbose_name="Регион")
     name_ru = models.CharField(max_length=100, verbose_name="Русское название")
-    name_en = models.CharField(max_length=100, verbose_name="Латинское название")
+    name_en = models.CharField(max_length=100, unique=True, verbose_name="Латинское название")
     order = models.PositiveSmallIntegerField(default=0, verbose_name="Порядковый номер")
     image = models.ImageField(blank=True, verbose_name="Изображение", upload_to="cities")
 
--- countries/models.py
+++ countries/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Country(models.Model):
 	name_ru = models.CharField(max_length=100, verbose_name="Русское название")
-	name_en = models.CharField(max_length=100, verbose_name="Латинское название")
+	name_en = models.CharField(max_length=100, unique=True, verbose_name="Латинское название")
 	order = models.PositiveSmallIntegerField(default=0, verbose_name="Порядковый номер")
 	image = models.ImageField(blank=True, verbose_name="Изображение", upload_to="countries")
 
