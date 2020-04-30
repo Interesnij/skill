3fd35b5654b5f4e59b0d6e73e8ee460903674221
Serg <ochkarik1983@mail.ru@>
2020-04-30 22:32:28 +0300

    G

---
+++ users/templatetags/is_favorite.py
@@ -0,0 +1,24 @@
+from django import template
+register=template.Library()
+
+
+@register.filter
+def is_ad_favorite(user, ad_id):
+    if user.is_ad_in_favorite(ad_id):
+        return True
+    else:
+        return False
+
+@register.filter
+def is_course_favorite(user, course_id):
+    if user.is_course_in_favorite(course_id):
+        return True
+    else:
+        return False
+
+@register.filter
+def is_anketa_favorite(user, anketa_id):
+    if user.is_anketa_in_favorite(anketa_id):
+        return True
+    else:
+        return False
