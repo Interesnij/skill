f1768f23f009cb403e025b50667bd535d035e1d3
Serg <ochkarik1983@mail.ru@>
2020-04-30 16:12:48 +0300

    G

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from users.views import *
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', ProfileUserView.as_view(), name='user'),
     url(r'^(?P<pk>\d+)/posts/$', UserAdsView.as_view(), name='user_ads'),
-    url(r'^(?P<pk>\d+)/courses/$', UserCoursesView.as_view(), name='user_courses'),
+    url(r'^(?P<pk>\d+)/courses/$', UserCoursesView.as_view(), name='my_courses'),
     url(r'^(?P<pk>\d+)/ankets/$', UserAnketsView.as_view(), name='user_ankets'),
     url(r'^(?P<pk>\d+)/my_favorites/$', UserFavoriteView.as_view(), name='user_favorite'),
     url(r'^(?P<pk>\d+)/my_subscribe/$', MySubscribeView.as_view(), name='my_subscribes'),
--- users/views.py
+++ users/views.py
@@ -40,12 +40,12 @@ class UserAdsView(ListView):
 
 
 class UserCoursesView(ListView):
-    template_name = "list/courses.html"
+    template_name = None
     paginate_by = 30
 
     def get(self,request,*args,**kwargs):
         self.user=User.objects.get(pk=self.kwargs["pk"])
-        #self.template_name = self.user.get_my_template(folder="list/", template="courses.html", request=request)
+        self.template_name = self.user.get_my_template(folder="list/", template="courses.html", request=request)
         return super(UserCoursesView,self).get(request,*args,**kwargs)
 
     def get_context_data(self, **kwargs):
