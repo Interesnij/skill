c1105eff28411e18a5c2cda0db0252026f7129a6
Serg <ochkarik1983@mail.ru@>
2020-05-01 10:44:19 +0300

    H

--- users/models.py
+++ users/models.py
@@ -249,7 +249,7 @@ class User(AbstractUser):
     def get_my_favorite_ads(self):
         from ad_posts.models import Ad
 
-        favorites_query = Q(ad_favorites__user_id=self.id, is_active=True, is_sold=False, is_deleted=False)
+        favorites_query = Q(adfavourites__user_id=self.id, is_active=True, is_sold=False, is_deleted=False)
         favorites_query.add(~Q(Q(creator__blocked_by_users__blocker_id=self.id) | Q(creator__user_blocks__blocked_user_id=self.id)), Q.AND)
         favorites = Ad.objects.filter(favorites_query)
         return favorites
