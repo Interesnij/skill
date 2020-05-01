4814b354c71a28827c97ca515c2ca139b025b66a
Serg <ochkarik1983@mail.ru@>
2020-05-01 14:49:47 +0300

    H

--- static/scripts/user_post.js
+++ static/scripts/user_post.js
@@ -1,9 +1,9 @@
 on('body', 'click', '.user_subscribe', function(e) {
   e.preventDefault();
+  parent = this.parent;
   send_change(this, "/users/progs/subscribe/", "user_unsubscribe", "Отписаться");
-    next_block = this.parent.nextElementSibling;
   try{
-    target = next_block.querySelector(".user_unblock");
+    target = parent.nextElementSibling.querySelector(".user_unblock");
     send_change(target, "/users/progs/unblock/", "user_unsubscribe", "Отписаться")
   }catch{null}
 })
@@ -15,10 +15,10 @@ on('body', 'click', '.user_unsubscribe', function(e) {
 
 on('body', 'click', '.user_block', function(e) {
   e.preventDefault();
+  parent = this.parent;
   send_change(this, "/users/progs/block/", "user_unblock", "Разблокировать");
-  previous_block = this.parent.previousElementSibling;
   try{
-  target = previous_block.querySelector(".user_unsubscribe");
+  target = parent.previousElementSibling.querySelector(".user_unsubscribe");
   target.classList.add(".user_subscribe");
   target.classList.remove(".user_unsubscribe");
   target.innerHTML = "Подписаться";
