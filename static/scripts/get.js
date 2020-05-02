9be3f09c12a36c3fad4e3902d336513a0493f61f
Serg <ochkarik1983@mail.ru@>
2020-05-02 13:17:29 +0300

    G

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -15,7 +15,7 @@ on('body', 'click', '.more_search_fields', function(e) {
   this.nextElementSibling.classList.toggle("show");
 });
 
-$('body').on('click', '#image-holder', function() {
+$('body').on('click', '#image-holde', function() {
  img = $(this);
 entrou = false;
  imageLoader = img.prev();
@@ -42,3 +42,42 @@ imageLoader.click();
 setTimeout(function() { entrou = false; }, 1000);
 img.prev().prev().show();
 console.log("upload comment image 2")});});
+
+
+var image_Loader = document.getElementById("id_image");
+var entrou = false;
+$("#image-holder").on("click", function() {
+$("#id_image").click();
+});
+
+$("#id_image").on("change", function() {
+if (!entrou) {
+  var imgPath = $(this)[0].value;
+  var extn = imgPath.substring(imgPath.lastIndexOf(".") + 1).toLowerCase();
+
+  if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
+    if (typeof FileReader != "undefined") {
+      var image_holder = $("#image-holder");
+      image_holder.empty();
+
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $img = $("<img />", {
+          id: "targetImageCrop",
+          src: e.target.result,
+          class: "thumb-image"
+        }).appendTo(image_holder);
+      };
+
+      image_holder.show();
+      reader.readAsDataURL($(this)[0].files[0]);
+    }
+  } else {
+    this.value = null;
+  }
+}
+entrou = true;
+setTimeout(function() {
+  entrou = false;
+}, 1000);
+});
