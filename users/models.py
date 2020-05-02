73dc77ffb4b399e4da5882bf37b09300e3ede59a
Serg <ochkarik1983@mail.ru@>
2020-05-02 17:31:15 +0300

    G

--- users/models.py
+++ users/models.py
@@ -25,6 +25,9 @@ class User(AbstractUser):
     def get_full_name(self):
         return  str(self.first_name) + " " + str(self.last_name)
 
+    def __str__(self):
+        return  self.get_full_name()
+
     def get_online(self):
         from datetime import datetime, timedelta
 
