7678656a6c45878ba1c62ce995200d643a997a54
Serg <ochkarik1983@mail.ru@>
2020-05-02 13:14:58 +0300

    G

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -15,32 +15,30 @@ on('body', 'click', '.more_search_fields', function(e) {
   this.nextElementSibling.classList.toggle("show");
 });
 
-on('body', 'click', '#image-holder', function() {
-img = this;
+$('body').on('click', '#image-holder', function() {
+ img = $(this);
 entrou = false;
-var imageLoader = document.querySelector("#id_image");
+ imageLoader = img.prev();
 imageLoader.click();
-on('body', 'change', imageLoader, function() {
-  if (!entrou) {
-    imgPath = this.value;
+ $(imageLoader).on("change", function() {
+  if (!entrou) {imgPath = $(this)[0].value;
     extn = imgPath.substring(imgPath.lastIndexOf(".") + 1).toLowerCase();
-    if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg")
-    {if (typeof FileReader != "undefined") {
-      image_holder = this;
-      reader = new FileReader();
-      reader.onload = function(e) {
-        $img = document.createElement("img"),
-        $img.id = "targetImageCrop";
-        $img.src = e.target.result;
-        $img.class = "thumb-image";
-        image_holder.append($img);
-        image_holder.style.display == "block";
-      }
-        reader.readAsDataURL(this.files);
+  if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg")
+  {if (typeof FileReader != "undefined") {
+    image_holder = $(img);
+    image_holder.empty();
+    reader = new FileReader();
+    reader.onload = function(e) {
+      $img = $("<img />", {
+        id: "targetImageCrop",
+        src: e.target.result,
+        class: "thumb-image" }).appendTo(image_holder);
+      };
+      image_holder.show();
+      reader.readAsDataURL($(this)[0].files[0]);
     }
-     else {this.value = null;}
-  } entrou = true;
-  setTimeout(function() { entrou = false; }, 1000);
-}
-});
-});
+  } else { this.value = null; }
+} entrou = true;
+setTimeout(function() { entrou = false; }, 1000);
+img.prev().prev().show();
+console.log("upload comment image 2")});});
