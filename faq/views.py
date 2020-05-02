f7808ba5d22c1deffc3a588951943a17ce0ad816
Serg <ochkarik1983@mail.ru@>
2020-05-02 14:48:28 +0300

    G

--- faq/views.py
+++ faq/views.py
@@ -8,17 +8,19 @@ class FaqView(ListView):
     paginate_by = 30
 
     def get(self,request,*args,**kwargs):
-		import re
+        import re
 
-		if not request.user.is_deleted:
-			self.template_name = "faq/faq.html"
-		else:
-			self.template_name = "generic/user_deleted.html"
+        if not request.user.is_deleted and not request.user.is_blocked:
+            self.template_name = "faq/faq.html"
+        elif request.user.is_deleted:
+            self.template_name = "generic/user_deleted.html"
+        elif request.user.is_blocked:
+            self.template_name = "generic/you_are_block.html"
 
-		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
-		if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
-			self.template_name = "mob_" + self.template_name
-		return super(FaqView,self).get(request,*args,**kwargs)
+        MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+        if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+            self.template_name = "mob_" + self.template_name
+        return super(FaqView,self).get(request,*args,**kwargs)
 
     def get_queryset(self):
         quans = Quan.models.only("pk")
