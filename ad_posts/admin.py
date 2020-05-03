7c847beb8c6a1f3e24df730c594d3d6a9a3e8750
Serg <ochkarik1983@mail.ru@>
2020-05-03 11:45:39 +0300

    F

--- ad_posts/admin.py
+++ ad_posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from ad_posts.models import Ad, AdFavourites
+from ad_posts.models import Ad, AdFavourites, AdDesign
 
 
 admin.site.register(Ad)
 admin.site.register(AdFavourites)
+admin.site.register(AdDesign)
--- users/models.py
+++ users/models.py
@@ -128,7 +128,7 @@ class User(AbstractUser):
                 AdNumbers.objects.create(user=request.user.pk, ad=ad.pk, platform=0)
         return template_name
 
-    def get_template_course_detail(self, course, folder, template, request):
+    def get_template_course_detail(self, folder, template, request):
         import re
         from stst.models import CourseNumbers
 
@@ -159,6 +159,39 @@ class User(AbstractUser):
         else:
             if request.user.is_authenticated and course.creator.pk != request.user.pk:
                 CourseNumbers.objects.create(user=request.user.pk, course=course.pk, platform=0)
+        return template_name
+
+    def get_template_anketa_detail(self, folder, template, request):
+        import re
+        from stst.models import AnketaNumbers
+
+        if course.creator.pk == request.user.pk:
+            if not request.user.is_phone_verified:
+                template_name = "main/phone_verification.html"
+            else:
+                template_name = folder + "my_" + template
+        elif request.user.pk != course.creator.pk and request.user.is_authenticated:
+            if not request.user.is_phone_verified:
+                template_name = "main/phone_verification.html"
+            elif request.user.is_blocked_with_user_with_id(user_id=self.pk):
+                template_name = "generic/you_are_block.html"
+            elif request.user.is_deleted:
+                template_name = "generic/deleted_page.html"
+            elif request.user.is_blocked:
+                template_name = "generic/blocked_page.html"
+            else:
+                template_name = folder + template
+        elif request.user.is_anonymous:
+            template_name = folder + "anon_" + template
+
+        MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+        if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+            template_name = "mob_" + template_name
+            if request.user.is_authenticated and course.creator.pk != request.user.pk:
+                AnketaNumbers.objects.create(user=request.user.pk, anketa=anketa.pk, platform=1)
+        else:
+            if request.user.is_authenticated and course.creator.pk != request.user.pk:
+                AnketaNumbers.objects.create(user=request.user.pk, anketa=anketa.pk, platform=0)
         return template_name
 
     def is_adding_user_with_id(self, user_id):
