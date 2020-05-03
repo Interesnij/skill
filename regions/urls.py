30ef9b8956331045145cb6d8fe792c187f99eba7
Serg <ochkarik1983@mail.ru@>
2020-05-03 16:52:05 +0300

    F

--- regions/urls.py
+++ regions/urls.py
@@ -5,7 +5,7 @@ from regions.views import RegionView, AdsRegionView, CoursesRegionView, AnketsRe
 urlpatterns = [
     url(r'^(?P<region_name>[\w\-]+)/$', RegionView.as_view(), name='region'),
 
-    url(r'ads/(?P<region_name>[\w\-]+)/$', AdsRegionView.as_view(), name="ads_region"),
-	url(r'courses/(?P<region_name>[\w\-]+)/$', CoursesRegionView.as_view(), name="courses_region"),
-	url(r'ankets/(?P<region_name>[\w\-]+)/$', AnketsRegionView.as_view(), name="ankets_region"),
+    url(r'^ads/(?P<region_name>[\w\-]+)/$', AdsRegionView.as_view(), name="ads_region"),
+	url(r'^courses/(?P<region_name>[\w\-]+)/$', CoursesRegionView.as_view(), name="courses_region"),
+	url(r'^ankets/(?P<region_name>[\w\-]+)/$', AnketsRegionView.as_view(), name="ankets_region"),
 ]
