80bf66e1952da1fa28692b207f7e8e4ce3f3feb7
Serg <ochkarik1983@mail.ru@>
2020-05-04 13:42:06 +0300

    H

--- cities/urls.py
+++ cities/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^(?P<city_name>[\w\-]+)/$', CityView.as_view(), name='city'),
     url(r'^load/(?P<pk>\d+)/$', GetCitiesView.as_view()),
 
-    url(r'^ads/(?P<region_name>[\w\-]+)/$', AdCityView.as_view(), name="ads_city"),
+    url(r'^ads/(?P<city_name>[\w\-]+)/$', AdCityView.as_view(), name="ads_city"),
 	url(r'^courses/(?P<city_name>[\w\-]+)/$', CoursesCityView.as_view(), name="courses_city"),
 	url(r'^ankets/(?P<city_name>[\w\-]+)/$', AnketsCityView.as_view(), name="ankets_city"),
 ]
