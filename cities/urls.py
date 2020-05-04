5439cfa62257ff2242c6dbc59dafdfc79dd3b5ed
Serg <ochkarik1983@mail.ru@>
2020-05-04 13:20:48 +0300

    H

--- cities/urls.py
+++ cities/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
-from cities.views import CityView, GetCitiesView
+from cities.views import CityView, GetCitiesView, AdCitiesView, CoursesCitiesView, AnketsCitiesView
 
 
 urlpatterns = [
     url(r'^(?P<city_name>[\w\-]+)/$', CityView.as_view(), name='city'),
     url(r'^load/(?P<pk>\d+)/$', GetCitiesView.as_view()),
+
+    url(r'^ads/(?P<region_name>[\w\-]+)/$', AdCitiesView.as_view(), name="ads_city"),
+	url(r'^courses/(?P<city_name>[\w\-]+)/$', CoursesCitiesView.as_view(), name="courses_city"),
+	url(r'^ankets/(?P<city_name>[\w\-]+)/$', AnketsCitiesView.as_view(), name="ankets_city"),
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,40 +1,134 @@
+import re
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView
-from cities.models import City
 from regions.models import Region
-from ad_posts.models import Ad
-import re
-from ad_categories.models import AdCategory
+from cities.models import City
 
 
-class CityView(ListView):
-    template_name = None
-    model = Ad
-    paginate_by = 30
+class CitiesView(TemplateView):
+	template_name = None
 
-    def get(self,request,*args,**kwargs):
-        self.city = City.objects.get(name_en=self.kwargs["city_name"])
-        if request.user.is_authenticated and not request.user.is_deleted:
-            self.template_name = "city/page.html"
-        elif request.user.is_authenticated and request.user.is_deleted:
-            self.template_name = "generic/user_deleted.html"
-        elif request.user.is_anonymous:
-            self.template_name = "city/anon_page.html"
+	def get(self,request,*args,**kwargs):
+		import re
 
-        MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
-        if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
-            self.template_name = "mob_" + self.template_name
-        return super(CityView,self).get(request,*args,**kwargs)
+		self.city = City.objects.get(name_en=self.kwargs["city_name"])
+		if request.user.is_authenticated and not request.user.is_deleted:
+			self.template_name = "cities/city.html"
+		elif request.user.is_authenticated and request.user.is_deleted:
+			self.template_name = "generic/user_deleted.html"
+		elif request.user.is_anonymous:
+			self.template_name = "cities/anon_region.html"
 
-    def get_context_data(self, **kwargs):
-        context = super(CityView, self).get_context_data(**kwargs)
-        context['city'] = self.city
-        context['ad_categories'] = AdCategory.objects.only("pk")
-        return context
+		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+		if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+			self.template_name = "mob_" + self.template_name
+		return super(CitiesView,self).get(request,*args,**kwargs)
 
-    def get_queryset(self):
-        ads = self.city.get_ads()
-        return ads
+	def get_context_data(self, **kwargs):
+		context = super(CitiesView, self).get_context_data(**kwargs)
+		context['last_ads'] = self.city.get_last_ads()
+		context['last_courses'] = self.city.get_last_courses()
+		context['last_ankets'] = self.city.get_last_ankets()
+		context['city'] = self.city
+		return context
+
+
+class AdCitiesView(ListView):
+	from ad_posts.models import Ad
+
+	template_name = None
+	model = Ad
+	paginate_by = 30
+
+	def get(self,request,*args,**kwargs):
+		self.city = City.objects.get(name_en=self.kwargs["city_name"])
+		if request.user.is_authenticated and not request.user.is_deleted:
+			self.template_name = "cities/ads.html"
+		elif request.user.is_authenticated and request.user.is_deleted:
+			self.template_name = "generic/user_deleted.html"
+		elif request.user.is_anonymous:
+			self.template_name = "cities/anon_ads.html"
+
+		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+		if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+			self.template_name = "mob_" + self.template_name
+		return super(AdCitiesView,self).get(request,*args,**kwargs)
+
+	def get_context_data(self, **kwargs):
+		from ad_categories.models import AdCategory
+
+		context = super(AdCitiesView, self).get_context_data(**kwargs)
+		context['city'] = self.city
+		context['ad_categories'] = AdCategory.objects.only("pk")
+		return context
+	def get_queryset(self):
+		ads = self.city.get_ads()
+		return ads
+
+
+class CoursesCitiesView(ListView):
+	from skill_posts.models import Course
+
+	template_name = None
+	model = Course
+	paginate_by = 30
+
+	def get(self,request,*args,**kwargs):
+		self.city = City.objects.get(name_en=self.kwargs["city_name"])
+		if request.user.is_authenticated and not request.user.is_deleted:
+			self.template_name = "cities/courses.html"
+		elif request.user.is_authenticated and request.user.is_deleted:
+			self.template_name = "generic/user_deleted.html"
+		elif request.user.is_anonymous:
+			self.template_name = "cities/anon_courses.html"
+
+		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+		if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+			self.template_name = "mob_" + self.template_name
+		return super(CoursesCitiesView,self).get(request,*args,**kwargs)
+
+	def get_context_data(self, **kwargs):
+		from skill_categories.models import SkillCategory
+
+		context = super(CoursesCitiesView, self).get_context_data(**kwargs)
+		context['city'] = self.city
+		context['skill_categories'] = SkillCategory.objects.only("pk")
+		return context
+
+	def get_queryset(self):
+		courses = self.city.get_courses()
+		return courses
+
+
+class AnketsCitiesView(ListView):
+	from love_posts.models import Anketa
+
+	template_name = None
+	model = Anketa
+	paginate_by = 30
+
+	def get(self,request,*args,**kwargs):
+		self.city = City.objects.get(name_en=self.kwargs["city_name"])
+		if request.user.is_authenticated and not request.user.is_deleted:
+			self.template_name = "cities/ankets.html"
+		elif request.user.is_authenticated and request.user.is_deleted:
+			self.template_name = "generic/user_deleted.html"
+		elif request.user.is_anonymous:
+			self.template_name = "cities/ankets_anon.html"
+
+		MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+		if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+			self.template_name = "mob_" + self.template_name
+		return super(AnketsCitiesView,self).get(request,*args,**kwargs)
+
+	def get_context_data(self, **kwargs):
+		context = super(AnketsCitiesView, self).get_context_data(**kwargs)
+		context['city'] = self.city
+		return context
+
+	def get_queryset(self):
+		ankets = self.city.get_ankets()
+		return ankets
 
 
 class GetCitiesView(TemplateView):
