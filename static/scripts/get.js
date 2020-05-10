e2f819a45d05a6a7bd72fbc93ee2655738476bb4
Serg <ochkarik1983@mail.ru@>
2020-05-10 17:44:50 +0300

    H

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -36,7 +36,7 @@ on('body', 'click', '.change-class', function(e) {
   parentClass = "." + _this.getAttribute("data-parent-class");
   remove_class = _this.getAttribute("data-change-from-class");
   console.log(parentClass, remove_class);
-  parentClass.classlist.remove(remove_class);
+  parentClass.classlist.remove("'" + remove_class + "'");
   parentClass.classlist.add(_this.getAttribute("data-change-to-class"));
   _this.parentElement.querySelector(".change-class").classlist.remove("active");
   _this.classlist.add("active");
