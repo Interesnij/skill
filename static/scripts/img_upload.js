462a7a9d8b12671384a2b23e43075c23caa4ffeb
Serg <ochkarik1983@mail.ru@>
2020-05-10 16:15:35 +0300

    H

--- static/scripts/img_upload.js
+++ static/scripts/img_upload.js
@@ -20,7 +20,7 @@ img.onchange = function() {
       ggg.append($img)
       };
       //ggg.append($img);
-      reader.readAsDataURL(ggg.files[0]);
+      reader.readAsDataURL(ggg.files);
     }
   } else { this.value = null; }
 } entrou = true;
