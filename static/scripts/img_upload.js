82f22fa7f0cd4e7a3a3ca53ff53dce60b0c720ff
Serg <ochkarik1983@mail.ru@>
2020-05-10 13:29:37 +0300

    H

--- static/scripts/img_upload.js
+++ static/scripts/img_upload.js
@@ -1,11 +1,11 @@
 
-  var imageLoader = document.getElementById("id_images");
+  var imageLoader = document.getElementById("id_image");
 var entrou = false;
 $("#image-holder").on("click", function() {
   $("#id_image").click();
 });
 
-$("#id_images").on("change", function() {
+$("#id_image").on("change", function() {
   if (!entrou) {
     var imgPath = $(this)[0].value;
     var extn = imgPath.substring(imgPath.lastIndexOf(".") + 1).toLowerCase();
