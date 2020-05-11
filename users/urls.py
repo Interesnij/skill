878a186aa365c26a0f4b404bf372d840f5d59ab7
Serg <ochkarik1983@mail.ru@>
2020-05-11 18:26:06 +0300

    G

--- users/urls.py
+++ users/urls.py
@@ -17,5 +17,4 @@ urlpatterns = [
     url(r'^ad_progs/', include('users.url.ad_progs')),
     url(r'^skill_progs/', include('users.url.skill_progs')),
     url(r'^love_progs/', include('users.url.love_progs')),
-
 ]
