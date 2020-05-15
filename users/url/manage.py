b3a55329cb653816ebe5a4bdbf4c15e9d9cd8b85
Serg <ochkarik1983@mail.ru@>
2020-05-15 13:59:43 +0300

    H

--- users/url/manage.py
+++ users/url/manage.py
@@ -9,8 +9,8 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    url(r'^module/(?P<pk>\d+)/$', login_required(GetAdManage.as_view()), name="get_module_staff")
-    url(r'^skill/(?P<pk>\d+)/$', login_required(GetSkillManage.as_view()), name="get_skill_staff"),
+    url(r'^module/(?P<pk>\d+)/$', login_required(GetAdManage.as_view()), name="get_module_staff"),
+    url(r'^skill/(?P<pk>\d+)/$', login_required(GetSkillManage.as_view()), name="get_skill_staff")
     #url(r'^ankets/(?P<pk>\d+)/$', login_required(GetAnketsManage.as_view()), name="get_ankets_staff"),
     #url(r'^staff/(?P<pk>\d+)/$', login_required(GetStaffManage.as_view()), name="get_users_staff"),
 ]
