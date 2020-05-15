c39326b2a6e91f144aaad9b7d70dc62f23447458
Serg <ochkarik1983@mail.ru@>
2020-05-15 20:29:26 +0300

    H

--- users/admin.py
+++ users/admin.py
@@ -10,7 +10,9 @@ from users.model.profile import (
                                     UserAnketaStaff,
                                     CanAddStaffAdUser,
                                     CanAddStaffSkillUser,
-                                    CanAddStaffAnketaUser
+                                    CanAddStaffAnketaUser,
+                                    CanAddStaffUser,
+                                    UserStaff
                                 )
 from users.model.list import ChatAd, Subscribe, Guest, UserBlock
 from django.conf import settings
@@ -24,12 +26,16 @@ admin.site.register(IPUser)
 admin.site.register(OneUserLocation)
 admin.site.register(TwoUserLocation)
 admin.site.register(ThreeUserLocation)
+
 admin.site.register(UserAdStaff)
 admin.site.register(UserSkillStaff)
 admin.site.register(UserAnketaStaff)
+admin.site.register(UserStaff)
+
 admin.site.register(CanAddStaffAdUser)
 admin.site.register(CanAddStaffSkillUser)
 admin.site.register(CanAddStaffAnketaUser)
+admin.site.register(CanAddStaffUser)
 
 admin.site.register(ChatAd)
 admin.site.register(Subscribe)
