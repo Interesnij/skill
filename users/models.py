4c93f9f2cda5ec37e5eac3a9eb195ccb2fbaf080
Serg <ochkarik1983@mail.ru@>
2020-05-15 18:08:11 +0300

    H

--- users/models.py
+++ users/models.py
@@ -401,8 +401,18 @@ class User(AbstractUser):
         return subscribs
 
     def get_ads_managers(self):
-        from users.model.profile import CanAddStaffAdUser
-        managers = CanAddStaffAdUser.objects.filter(Q(Q(can_work_administrator=True) | Q(can_work_moderator=True) | Q(can_work_editor=True) | Q(can_work_advertiser=True))).values('user_id')
+        from users.model.profile import UserAdStaff
+        managers = UserAdStaff.objects.filter(Q(Q(can_work_administrator=True) | Q(can_work_moderator=True) | Q(can_work_editor=True) | Q(can_work_advertiser=True))).values('user_id')
+        managers_ids = [user['user_id'] for user in managers]
+        users = Q(id__in=managers_ids)
+        exclude_superuser = ~Q(is_superuser=True)
+        users.add(exclude_superuser, Q.AND)
+        managers_query = User.objects.filter(users)
+        return managers_query
+
+    def get_skills_managers(self):
+        from users.model.profile import UserSkillStaff
+        managers = UserSkillStaff.objects.filter(Q(Q(can_work_administrator=True) | Q(can_work_moderator=True) | Q(can_work_editor=True) | Q(can_work_advertiser=True))).values('user_id')
         managers_ids = [user['user_id'] for user in managers]
         users = Q(id__in=managers_ids)
         exclude_superuser = ~Q(is_superuser=True)
