f403da184b7193417a5226079306422dd4f2d003
Serg <ochkarik1983@mail.ru@>
2020-05-16 12:46:04 +0300

    G

--- static/scripts/user_post.js
+++ static/scripts/user_post.js
@@ -1,12 +1,12 @@
 on('body', 'click', '#register_ajax', function() {
   form_data = new FormData(document.querySelector("#signup"));
-  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
-  link.open( 'POST', "/rest-auth/registration/", true );
-  link.onreadystatechange = function () {
-  if ( link.readyState == 4 && link.status == 200 ) {
+  reg_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  reg_link.open( 'POST', "/rest-auth/registration/", true );
+  reg_link.onreadystatechange = function () {
+  if ( reg_link.readyState == 4 && reg_link.status == 200 ) {
     window.location.href = "/phone_send/";
     }};
-  link.send(form_data);
+  reg_link.send(form_data);
 })
 
 
