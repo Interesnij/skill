b8deaac32405fe7c41df4a8c9edd6021812c2733
Serg <ochkarik1983@mail.ru@>
2020-05-17 15:28:42 +0300

    F

--- static/scripts/img_upload.js
+++ static/scripts/img_upload.js
@@ -24,5 +24,35 @@ img.onchange = function() {
     }
   } else { this.value = null; }
 } entrou = true;
-setTimeout(function() { entrou = false; }, 1000)
+setTimeout(function() { entrou = false; }, 1000);
+ggg.parentElement.nextElementSibling.style.display = "block";
+}});
+
+on('body', 'click', '#image-holder1', function() {
+entrou = false;
+ggg = this;
+img = this.previousElementSibling.querySelector("#id_img1");
+img.click();
+
+img.onchange = function() {
+  if (!entrou) {imgPath = img.value;
+    extn = imgPath.substring(imgPath.lastIndexOf(".") + 1).toLowerCase();
+  if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg")
+  {if (typeof FileReader != "undefined") {
+    ggg.innerHTML = "";
+    reader = new FileReader();
+    reader.onload = function(e) {
+      $img = document.createElement("img");
+      $img.id = "targetImageCrop";
+      $img.src = e.target.result;
+      $img.class = "thumb-image";
+      ggg.append($img);
+
+      };
+      reader.readAsDataURL(img.files[0]);
+    }
+  } else { this.value = null; }
+} entrou = true;
+setTimeout(function() { entrou = false; }, 1000);
+ggg.parentElement.nextElementSibling.style.display = "block";
 }});
